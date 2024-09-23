69eb3b724218d407ebf6f0ae4575b745253ddfa4
Seif Sameh <seifsameh2626@gmail.com>
2024-09-23 23:50:13 +0300

    initail

--- front/src/Components/SellerDashboard.jsx
+++ front/src/Components/SellerDashboard.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { Link, useLocation } from 'react-router-dom';
+import { Link, } from 'react-router-dom';
 import axios from 'axios'
+import { GoTrash } from "react-icons/go";
 
 const sellerDashboard = ({ name, id }) => {
 
   const [found, setFound] = useState(false)
   const [products, setProducts] = useState([])
+
+  const [ordersFound, setOrdersFound] = useState(false)
+  const [orders, setOrders] = useState([])
 
   const retriveProducts = () => {
     axios.post('http://localhost/e-commerce/seller_products.php', { name: name, id: id })
@@ -17,9 +21,29 @@ const sellerDashboard = ({ name, id }) => {
         }
       })
   }
+  const fetchOrders = () => {
+    axios.post('http://localhost/e-commerce/seller_orders.php', { name: name, id: id })
+      .then((res) => (res.data))
+      .then((data) => {
+        if (data.status == 'OK') {
+          setOrdersFound(data.found)
+          setOrders(data.products)
+        }
+      })
+  }
+  const removeProduct = (product_id) => {
+    axios.post('http://localhost/e-commerce/remove_product.php', { name: name, id: id, product_id })
+      .then((res) => (res.data))
+      .then((data) => {
+        if (data.status == 'OK') {
+          retriveProducts()
+        }
+      })
+  }
 
   useEffect(() => {
     retriveProducts()
+    fetchOrders()
   }, [])
 
   return (
@@ -36,18 +60,23 @@ const sellerDashboard = ({ name, id }) => {
         <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 max-sm:grid-cols-1 gap-4'>
           {
             found && products.map((item) => (
-              <div key={item.id} className='p-2 flex flex-col gap-4'>
+              <div key={item.id} className='p-2 flex flex-col gap-4 h-[400px]'>
                 <div className='w-full h-1/2'>
                   <img src={`${item.image_path}`} alt="" className='w-full h-full rounded-lg' />
                 </div>
-                <div className='w-full h-2/5 flex flex-col justify-between px-2 gap-4'>
-                  <div className='flex flex-col gap-4'>
+                <div className='w-full h-2/5 flex flex-col justify-between px-2 gap-2'>
+                  <div className='flex flex-col gap-2'>
                     <p className='text-xl font-semibold'>{item.name}</p>
                     <p className='text-sm line-clamp-2'>{item.description}</p>
                   </div>
-                  <div className='flex justify-between'>
-                    
-                    <p className='text-xl font-bold'>SAR {item.product_price} </p>
+                  <div className='h-full text-sm flex items-center justify-between'>
+                    <p className='text-xl font-bold'>{item.product_price} SAR</p>
+                    <div className='flex items-center gap-1 cursor-pointer' onClick={() => {
+                      removeProduct(item.id)
+                    }}>
+                      <GoTrash />
+                      <span>حذف</span>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -55,6 +84,10 @@ const sellerDashboard = ({ name, id }) => {
           }
         </div>
       </div>
+      <div className='flex justify-between px-10'>
+        <p className='text-3xl font-bold'>الطلبات</p>
+      </div>
+
     </div>
   )
 }
