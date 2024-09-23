f952e873baeb7d00ae4d6562cbd109fcd46d7676
Seif Sameh <seifsameh2626@gmail.com>
2024-09-23 21:37:30 +0300

    initail

--- front/src/Components/Cart.jsx
+++ front/src/Components/Cart.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FiSearch } from "react-icons/fi";
 import { FaUser } from "react-icons/fa6";
 import { FiArrowLeft } from "react-icons/fi";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { GoTrash } from "react-icons/go";
 
 const Cart = () => {
-  return (
-    <div className='w-full flex flex-col  gap-14 pt-[50px]'>
+
+    const location = useLocation()
+    const { state } = location
+    const [cartItems, setCartItems] = useState([])
+    const [checkoutItems, setCheckoutItems] = useState([])
+
+    useEffect(() => {
+        setCartItems(state.cart_items)
+        setCheckoutItems(state.cart_items.map((item) => ({...item, quantity: 1})))
+    }, [])
+
+    console.log(cartItems)
+    console.log(checkoutItems)
+
+    return (
+        <div className='w-full flex flex-col  gap-14 pt-[50px]'>
             <div className='w-full px-10 flex justify-between items-center'>
                 <div className='w-1/4'>
 
@@ -23,33 +38,69 @@ const Cart = () => {
                 </div>
             </div>
 
-            <div className='w-full flex rounded-t-lg '>
-                <div className='w-2/5 p-8 h-full bg-gray-300 flex flex-col justify-between'>
+            <div className='w-full min-h-[60vh] flex max-md:flex-col-reverse rounded-t-lg '>
+                <div className='w-2/5 max-md:w-full p-8 h-full md:min-h-[60vh] bg-gray-300 flex flex-col justify-between'>
                     <div className='flex flex-col'>
                         <h1 className='font-semibold text-[50px]'>طلبك</h1>
 
                     </div>
                     <button className='bg-black py-2 rounded-md text-white text-lg'>الدفع </button>
                 </div>
-                <div className='w-3/5 p-8 h-full bg-white flex flex-col justify-between'>
-                    <div className='flex flex-col'>
+                <div className='w-3/5 max-md:w-full py-10 pr-16 pl-8 h-full md:min-h-[60vh] bg-white rounded-t-md flex flex-col justify-between '>
+                    <div className='flex flex-col mb-4'>
                         <div className='flex justify-between items-center'>
-                        <h1 className='font-semibold text-[50px]'>سلتك</h1>
-                        <Link to={'/products'}>
-                        <div className='flex gap-2 items-end'>
-                            <p>استكمال التسوق</p>
-                            <FiArrowLeft className='mb-1'/>
+                            <h1 className='font-semibold text-[50px]'>سلتك</h1>
+                            <Link to={'/products'} state={{ cart_items: cartItems }}>
+                                <div className='flex gap-2 items-end'>
+                                    <p>استكمال التسوق</p>
+                                    <FiArrowLeft className='mb-1' />
+                                </div>
+                            </Link>
                         </div>
-                        </Link>
-                        </div>
-
                     </div>
-                
+                    <div className='flex flex-col divide-y-2'>
+                        {
+                            cartItems && cartItems.map((item) => (
+                                <div key={item.id} className='w-full h-[200px] p-4 flex items-center gap-6'>
+                                    <div className='w-[250px] h-full flex items-center'>
+                                        <img src={item.image_path} alt="" className='rounded-md h-full w-full' />
+                                    </div>
+                                    <div className='h-full flex-1 flex flex-col justify-between'>
+                                        <div className='flex flex-col gap-2'>
+                                            <p className='text-2xl font-bold'>{item.name}</p>
+                                            <p className='text-md text-gray-600'>{item.description}</p>
+                                        </div>
+
+                                        <div className='flex gap-2 items-center'>
+                                            <span>العدد</span>
+                                            <input type="number" name="" id="" defaultValue={1} className='w-[50px] bg-gray-300 py-1 px-2 rounded-md' 
+                                                onChange={(e) => {
+                                                    setCheckoutItems((prev) => prev.filter((p) => p.id != item.id))
+                                                    setCheckoutItems((prev) => [...prev, {...item, quantity: e.target.value}])
+                                                }}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className='h-full text-sm flex flex-col justify-between'>
+                                        <div className='flex items-center gap-1 cursor-pointer' onClick={() => {
+                                            setCartItems((prev) => prev.filter((p) => p.id != item.id))
+                                            setCheckoutItems((prev) => prev.filter((p) => p.id != item.id))
+                                        }}>
+                                            <GoTrash />
+                                            <span>حذف</span>
+                                        </div>
+                                        <p className='text-xl font-bold'>{item.description.slice(0, 7)}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+
                 </div>
             </div>
 
         </div>
-  )
+    )
 }
 
 export default Cart
--- front/src/Components/ProductsPage.jsx
+++ front/src/Components/ProductsPage.jsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { FiSearch } from "react-icons/fi";
 import { FaUser } from "react-icons/fa6";
-import { PiBowlFoodFill } from "react-icons/pi";
-import { GiCupcake } from "react-icons/gi";
-import { RiDrinksFill } from "react-icons/ri";
 import { FaCartShopping } from "react-icons/fa6";
 import { Link, useLocation } from 'react-router-dom';
 import axios from 'axios';
 
-const ProductsPage = ({name, id}) => {
+const ProductsPage = ({ name, id }) => {
 
     const [category, setCategory] = useState('all')
     const [found, setFound] = useState(false)
@@ -17,32 +14,36 @@ const ProductsPage = ({name, id}) => {
 
     const [cart, setCart] = useState([])
 
+    const location = useLocation()
+    const {state} = location 
+
     const retriveProducts = () => {
-        axios.post('http://localhost/e-commerce/retrieve_products.php', {name, id})
-        .then((res) => (res.data))
-        .then((data) => {
-            if(data.status == 'OK'){
-                setProducts(data.products)
-                setFilteredProducts(data.products)
-                setFound(data.found)
-            }
-        })
+        axios.post('http://localhost/e-commerce/retrieve_products.php', { name, id })
+            .then((res) => (res.data))
+            .then((data) => {
+                if (data.status == 'OK') {
+                    setProducts(data.products)
+                    setFilteredProducts(data.products)
+                    setFound(data.found)
+                }
+            })
     }
 
     useEffect(() => {
-        retriveProducts() 
+        retriveProducts()
+        setCart(state.cart_items)
     }, [])
 
     return (
         <div className='w-full flex flex-col  gap-14 pt-[50px]'>
             <div className='w-full px-10 flex justify-between items-center'>
                 <div className='w-1/4'>
-                <Link to={'/cart'} state={{cart_items : cart}}>
-                <div className='flex w-fit items-center gap-2 bg-black text-white px-5 py-1 rounded-md'>
-                    <FaCartShopping />
-                    <button >السلة</button>
-                </div>
-                </Link>
+                    <Link to={'/cart'} state={{ cart_items: cart }}>
+                        <div className='flex w-fit items-center gap-2 bg-black text-white px-5 py-1 rounded-md'>
+                            <FaCartShopping />
+                            <button >السلة</button>
+                        </div>
+                    </Link>
                 </div>
                 <div className='w-1/2 flex justify-center gap-3'>
                     <input type='text' className='w-[250px] h-[40px] rounded-md bg-gray-200 placeholder:text-gray-500 px-3' placeholder='بحث' />
@@ -90,19 +91,22 @@ const ProductsPage = ({name, id}) => {
                 </div> */}
                 <div className='w-full bg-white rounded-t-lg p-8 min-h-[60vh]'>
                     <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 max-sm:grid-cols-1 gap-4'>
-                        
+
                         {
                             found && filteredProducts.map((item) => (
-                                <div key={item.id} className='p-2 h-[300px] flex flex-col gap-2'>  
-                                    <div className='w-full h-3/5'>
-                                        <img src={`${item.image_path}`} alt="" className='w-full h-full rounded-lg'/>
+                                <div key={item.id} className='p-2 flex flex-col gap-4'>
+                                    <div className='w-full h-1/2'>
+                                        <img src={`${item.image_path}`} alt="" className='w-full h-full rounded-lg' />
                                     </div>
-                                    <div className='w-full h-2/5 flex flex-col justify-between '>
-                                    <div className='flex flex-col gap-1'>
-                                        <p className='text-lg font-semibold'>{item.name}</p>
-                                        <p className='text-sm line-clamp-2'>{item.description}</p>
-                                    </div>
-                                        <button className='bg-black text-white py-1 px-3 rounded-md w-fit cursor-pointer' onClick={()=> setCart((prev) => [...prev, item])}>اضف الي سلتك</button>
+                                    <div className='w-full h-2/5 flex flex-col justify-between px-2 gap-4'>
+                                        <div className='flex flex-col gap-4'>
+                                            <p className='text-xl font-semibold'>{item.name}</p>
+                                            <p className='text-sm line-clamp-2'>{item.description.slice(7, )}</p>
+                                        </div>
+                                        <div className='flex justify-between'>
+                                        <button className='bg-black text-white py-1 px-3 rounded-md w-fit cursor-pointer' onClick={() => setCart((prev) => [...prev, item])}>اضف الي سلتك</button>
+                                        <p className='text-xl font-bold'>{item.description.slice(0, 7)}</p>
+                                        </div>
                                     </div>
                                 </div>
                             ))
