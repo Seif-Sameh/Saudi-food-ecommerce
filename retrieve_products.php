bc0144f2999d656b12cf692759e4d1bc0806e266
youssef mostafa <yomu611@gmail.com>
2024-09-23 08:29:04 +0300

    user's products

---
+++ retrieve_products.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit(0);
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    include('./connection.php');
+    $get_products = $conn->prepare("SELECT product_id, category, product_name, product_description, image_path, seller_id, seller_name FROM products ORDER BY id DESC");
+    $get_products->execute();
+    $get_products->store_result();
+    if ($get_products->num_rows == 0) {
+        http_response_code(200);
+        echo json_encode(['status' => 'OK', 'found' => false]);
+        $get_products->close();
+        $conn->close();
+        exit(0);
+    }
+    $get_products->bind_result($id, $category, $name, $description, $image_path, $seller_id, $seller_name);
+    $products = [];
+    while ($get_products->fetch()) {
+        $products[] = array("id" => $id, "name" => $name, "category" => $category, "description" => $description, "image_path" => $image_path, "seller_id" => $seller_id, "seller_name" => $seller_name);
+    }
+    http_response_code(200);
+    echo json_encode(['status' => 'OK', 'found' => true, 'products' => $products]);
+    $get_products->close();
+    $conn->close();
+    exit(0);
+} else {
+    http_response_code(405);
+    echo json_encode(['status' => 'error', 'message' => 'Invalid method']);
+    exit(0);
+}
